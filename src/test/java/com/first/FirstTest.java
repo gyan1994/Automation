c71e44e3963bdd6c42e587e71b93eac5f9036a14
Gyanendra Sharma <gyansharma10@gmail.com>
2022-03-09 17:45:47 +0530

    Update FirstTest.java

--- src/test/java/com/first/FirstTest.java
+++ src/test/java/com/first/FirstTest.java
@@ -3,15 +3,15 @@ package com.first;
 import cucumber.api.java.en.Given;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class FirstTest {
 
     @Given("^Test giving$")
     public void test_giving() {
         System.out.println("Hello Cucumber");
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriverManager.phantomjs().setup();
+        WebDriver driver=new PhantomJSDriver();
         driver.get("https://www.google.com");
     }
 }
