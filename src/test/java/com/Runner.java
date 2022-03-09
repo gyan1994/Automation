56929027c032ecf0511b9641b0f25f1f05819753
Gyanendra Sharma <gyansharma10@gmail.com>
2022-03-09 17:23:26 +0530

    Update Runner.java

--- src/test/java/com/Runner.java
+++ src/test/java/com/Runner.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/feature",
-glue = "com/second")
+glue = "com/first")
 public class Runner {
 }
